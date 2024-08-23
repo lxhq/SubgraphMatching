65e1d3d63d0309d01d1fad40279dabf775cdc01b
lxheqiao <lxheqiao@163.com>
2024-08-23 19:11:44 +0800

    modify the process_file script to check and skip already exist outputs

--- process_files.sh
+++ process_files.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Directory containing the input files
-input_dir="/home/ubuntu/Document/workspace/SubgraphMatching/test/query_graph/pattern_graph_new/6voc/"
+input_dir="/home/ubuntu/Documents/workspace/SubgraphMatching/test/query_graph/pattern_graph_new/6voc/"
 
 # Function to process each file
 process_file() {
     input_file="$1"
-    output_file="/home/ubuntu/Document/workspace/SubgraphMatching/test/outputs/6voc/$(basename "$input_file")"
+    output_file="/home/ubuntu/Documents/workspace/SubgraphMatching/test/outputs/6voc/$(basename "$input_file")"
+
+    # Check if output file already exists
+    if [ -f "$output_file" ]; then
+        return
+    fi
+
     echo "Processing $input_file"
 
     # Run the processing command and save output to the corresponding file
-    /home/ubuntu/Document/workspace/SubgraphMatching/build/matching/SubgraphMatching.out \
-    -d ~/Document/workspace/SubgraphMatching/test/data_graph/web-spam-new.graph \
+    /home/ubuntu/Documents/workspace/SubgraphMatching/build/matching/SubgraphMatching.out \
+    -d ~/Documents/workspace/SubgraphMatching/test/data_graph/web-spam-new.graph \
     -filter GQL -order GQL -engine LFTJ -num MAX \
     -q "$input_file" > "$output_file"
 }
