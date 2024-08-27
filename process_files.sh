c80ceb8ba8ed94b18b74cca4301f36b2a8c5025a
lxheqiao <lxheqiao@163.com>
2024-08-28 00:42:38 +0800

    modify the script to process the lsc task

--- process_files.sh
+++ process_files.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 # Directory containing the input files
-input_dir="/home/ubuntu/Documents/workspace/SubgraphMatching/test/query_graph/pattern_graph_new/6voc/"
+prefix="/home/ubuntu/Documents/workspace/SubgraphMatching"
+input_dir="$prefix/test/query_graph/pattern_graph_new/"
+data_graph="$prefix/test/data_graph/web-spam-new.graph"
+output_dir="$prefix/test/outputs/"
+executable="$prefix/build/matching/SubgraphMatching.out"
+log_file="$prefix/task_log.txt"
 
 # Function to process each file
 process_file() {
     input_file="$1"
-    output_file="/home/ubuntu/Documents/workspace/SubgraphMatching/test/outputs/6voc/$(basename "$input_file")"
+    input_file_basename="$(basename "$input_file")"
+    output_file="$2/$(basename "$input_file")"
 
-    # Check if output file already exists
-    if [ -f "$output_file" ]; then
+    if grep -Fxq "$input_file_basename" "$5"; then
+        echo "Skipping already processed file: $input_file_basename"
         return
     fi
 
-    echo "Processing $input_file"
-
     # Run the processing command and save output to the corresponding file
-    /home/ubuntu/Documents/workspace/SubgraphMatching/build/matching/SubgraphMatching.out \
-    -d ~/Documents/workspace/SubgraphMatching/test/data_graph/web-spam-new.graph \
+    "$3" \
+    -d "$4" \
     -filter GQL -order GQL -engine LFTJ -num MAX \
     -q "$input_file" > "$output_file"
+
+    echo "$input_file_basename" >> "$5"
+    echo "Done: "$input_file_basename""
 }
 
 export -f process_file  # Export the function for parallel to use
 
 # Find all files in the input directory and process them in parallel
-find "$input_dir" -type f | parallel --line-buffer process_file {}
+find "$input_dir" -type f | parallel --line-buffer process_file {} "$output_dir" "$executable" "$data_graph" "$log_file"
